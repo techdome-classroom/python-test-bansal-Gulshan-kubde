0ec093d3b07f053b722515399df5efc16e0981d8
Gulshan-kubde <128044926+Gulshan-kubde@users.noreply.github.com>
2024-02-23 12:57:01 +0530

    Update program2.py
    add function body

--- program2.py
+++ program2.py
@@ -1,7 +1,27 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
+         """
         :type s: str
         :rtype: int
         """
-        pass
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        result = 0
+        i = 0
+        while i < len(s):
+            if i+1 < len(s) and roman_values[s[i]] < roman_values[s[i+1]]:
+                result += roman_values[s[i+1]] - roman_values[s[i]]
+                i += 2
+            else:
+                result += roman_values[s[i]]
+                i += 1
+                
+        return result
