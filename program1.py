6adb3e67f9b9bbf9672174ce9c263447dfb6d084
Gulshan-kubde <128044926+Gulshan-kubde@users.noreply.github.com>
2024-02-23 13:01:16 +0530

    Update program1.py
    add function body

--- program1.py
+++ program1.py
@@ -1,12 +1,25 @@
+    
 class Solution(object):
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        pass
-    
-
+        stack = []
+        brackets = {')': '(', ']': '[', '}': '{'}
+        
+        for char in s:
+            if char in brackets.values():  # Opening bracket
+                stack.append(char)
+            elif char in brackets.keys():  # Closing bracket
+                if not stack or stack[-1] != brackets[char]:
+                    return False
+                stack.pop()
+            else:
+                return False  # Invalid character
+        
+        return len(stack) == 0  # True if all brackets are matched
+        
 
 
   
